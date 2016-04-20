8f3131ee9251864a2d817e0fd3fe430b07fd3d67
benofben <ben.lackey@outlook.com>
2016-04-19 17:46:58 -0700

    fill in parameters

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-# You typically would start with deploying a cluster on GKE
-gcloud container clusters create <cluster-name>
+gcloud deployment-manager deployments create cluster --config cluster.yaml
 
-# Once your GKE clusters are up, you would need to provide the cluster information in cluster.yaml. This can be typically found by
-kubectl config view
-kubectl view cluster-info
-
-# Once you have the cluster,yaml configured with name, IP and password, you can deploy cluster-type using
-gcloud deployment-manager deployments create cluster-types --config cluster.yaml
-
-# This should bring up the GKE deployment that you should be able to see in Deployment-Manager tab
-
-# Now from the context, cassandra.yaml and datastax.yaml would require some configuration from uour end to deploy the solutions. These would go in jinja file(s)
-
-# To deploy these solutions on the existing cluster, you would use
-gcloud deployment-manager deployments create <datastax | cassandra> --config <datastax | cassandra>.yaml
+gcloud deployment-manager deployments create cassandra --config cassandra.yaml
