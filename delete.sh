184f053f7d8d0297007a95099a33ba73d0804f97
benofben <ben.lackey@outlook.com>
2016-05-05 16:31:50 -0700

    bash

--- delete.sh
+++ delete.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
+
 gcloud deployment-manager deployments update cassandra --config empty.yaml --delete-policy ABANDON
 gcloud -q deployment-manager deployments delete cassandra
 gcloud -q deployment-manager deployments delete cluster
-
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 gcloud deployment-manager deployments create cluster --config cluster.yaml
-
 gcloud deployment-manager deployments create cassandra --config cassandra.yaml
