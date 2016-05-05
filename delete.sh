324d8694c6875ae89271b27ec271171a3c7ba4d4
benofben <ben.lackey@outlook.com>
2016-05-05 15:06:21 -0700

    delete both objects

--- delete.sh
+++ delete.sh
@@ -1,3 +1,4 @@
 gcloud deployment-manager deployments update cassandra --config empty.yaml --delete-policy ABANDON
 gcloud deployment-manager deployments delete cassandra
+gcloud deployment-manager deployments delete cluster
 
