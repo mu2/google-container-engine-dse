0ca15054933a0fa413449a97674a3274897180ef
benofben <ben.lackey@outlook.com>
2016-05-11 13:24:58 -0700

    delete script consolidation

--- delete.sh
+++ delete.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "resources:" > empty.yaml
 gcloud deployment-manager deployments update datastax --config empty.yaml --delete-policy ABANDON
+rm empty.yaml
+
 gcloud -q deployment-manager deployments delete datastax
 gcloud -q deployment-manager deployments delete cluster
