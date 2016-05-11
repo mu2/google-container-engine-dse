06d4dfecbc5c8b45bbc3359c7d8efb454fa2ec71
benofben <ben.lackey@outlook.com>
2016-05-10 22:05:28 -0700

    script cleanup.  mostly ; hardcode dc name

--- scripts/dse.sh
+++ scripts/dse.sh
@@ -6,18 +6,16 @@ unzip master.zip
 cd install-datastax-master/bin
 
 cloud_type="google"
-apt-get install curl
-zone=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/zone" | grep -o [[:alnum:]-]*$)
-data_center_name=$zone;
-seed_nodes_dns_names=$SEED_NODE_SERVICE;
+seed_nodes_dns_names=$SEED_NODE_SERVICE
+data_center_name="dc0"
 
-echo "Configuring nodes with the settings:";
-echo cloud_type $cloud_type;
-echo seed_nodes_dns_names $seed_nodes_dns_names;
-echo data_center_name $data_center_name;
+echo "Configuring nodes with the settings:"
+echo cloud_type $cloud_type
+echo seed_nodes_dns_names $seed_nodes_dns_names
+echo data_center_name $data_center_name
 
-apt-get install curl
-apt-get install sudo
-./dse.sh $cloud_type $seed_nodes_dns_names $data_center_name;
+apt-get -y install curl
+apt-get -y install sudo
+./dse.sh $cloud_type $seed_nodes_dns_names $data_center_name
 
 sleep 3600
