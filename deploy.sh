a977e9e051d26ee130c87c27f53f0e8cfa544509
benofben <ben.lackey@outlook.com>
2016-05-13 12:50:45 -0700

    directory restructure

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+cd gke
 gcloud deployment-manager deployments create cluster --config cluster.yaml
 gcloud deployment-manager deployments create datastax --config datastax.yaml
+cd ..
