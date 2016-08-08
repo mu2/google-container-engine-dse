dd8fa6a7eb4a6124e87e1cedcba18952c32356f1
benofben <ben.lackey@outlook.com>
2016-08-08 15:30:41 -0700

    deprecation warning

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd gke
-gcloud deployment-manager deployments create cluster --config cluster.yaml
-gcloud deployment-manager deployments create datastax --config datastax.yaml
+gcloud deployment-manager deployments create cluster --configuration cluster.yaml
+gcloud deployment-manager deployments create datastax --configuration datastax.yaml
 cd ..
