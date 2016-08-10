142339cdaf6f6316d603e5bb9ba7cfa212b01f6b
benofben <ben.lackey@outlook.com>
2016-08-09 17:28:14 -0700

    pass opsc name to work around agent issue

--- scripts/dse.sh
+++ scripts/dse.sh
@@ -16,13 +16,15 @@ cd install-datastax-5.0.1-1/bin
 cloud_type="gke"
 seed_nodes_dns_names=$SEED_NODE_SERVICE
 data_center_name="dc0"
+opscenter_dns_name=$OPSCENTER_NAME
 
 echo "Configuring nodes with the settings:"
 echo cloud_type $cloud_type
 echo seed_nodes_dns_names $seed_nodes_dns_names
 echo data_center_name $data_center_name
+echo opscenter_dns_name $opscenter_dns_name
 
-./dse.sh $cloud_type $seed_nodes_dns_names $data_center_name
+./dse.sh $cloud_type $seed_nodes_dns_names $data_center_name $opscenter_dns_name
 
 # 100 hours
 sleep 360000
