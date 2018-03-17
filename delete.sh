3260693de045c930c4f7b34ba37f8be4c2c38fbf
Gilbert Lau <gmflau@gmail.com>
2018-03-16 17:48:09 -0700

    Interim check-in

--- delete.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "resources:" > empty.yaml
-gcloud deployment-manager deployments update datastax --config empty.yaml --delete-policy ABANDON
-rm empty.yaml
-
-gcloud -q deployment-manager deployments delete datastax
-gcloud -q deployment-manager deployments delete cluster
--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd gke
-gcloud deployment-manager deployments create cluster --config cluster.yaml
-gcloud deployment-manager deployments create datastax --config datastax.yaml
-cd ..
